8fffec1d2a635bddc2b18b840384ef64ff210b2f
Jeff Auriemma <jdauriemma@gmail.com>
2019-04-02 00:03:47 -0400

    Initial commit with auth

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+const {
+  rewireBlockstackBuild,
+  rewireBlockstackDevServer,
+} = require('react-app-rewire-blockstack');
+
+module.exports = {
+  webpack: (config, env) => {
+    if (env === 'production') {
+      config = rewireBlockstackBuild(config);
+    }
+    return config;
+  },
+
+  devServer: (configFunction) => {
+    return (proxy, allowedHost) => {
+      let config = configFunction(proxy, allowedHost);
+      config = rewireBlockstackDevServer(config);
+      return config;
+    };
+  },
+}
--- src/App.js
+++ src/App.js
@@ -1,28 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  useState,
+  useEffect,
+} from 'react';
+import {
+  signUserOut,
+  loadUserData,
+  isUserSignedIn,
+  isSignInPending,
+  redirectToSignIn,
+  handlePendingSignIn,
+} from 'blockstack';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  const signIn = () => {
+    redirectToSignIn();
+  };
+
+  const signOut = () => {
+    signUserOut(window.location.origin);
+  };
+
+  const verifyAuthStatus = async () => {
+    if (isUserSignedIn()) {
+      const { profile } = loadUserData();
+      setUser(profile);
+    } else if (isSignInPending()) {
+      try {
+        const { profile } = await handlePendingSignIn();
+        setUser(profile);
+      } catch (e) {
+        console.error(e);
+        alert('obi-c has some problems');
+      }
+    }
+  };
+
+  useEffect(() => {
+    verifyAuthStatus();
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {
+          user ?
+            <div role='form'>
+              <button
+                id='sign-out'
+                className='btn'
+                onClick={signOut}
+              >
+                Sign out
+              </button>
+            </div> :
+            <div role='form'>
+              <button
+                id='sign-in'
+                className='btn'
+                onClick={signIn}
+              >
+                Sign in
+              </button>
+            </div>
+        }
+      </header>
+      <section>
+        <pre>
+          <code>
+            {
+              JSON.stringify(user || {}, null, 2)
+            }
+          </code>
+        </pre>
+      </section>
+    </div>
+  );
+};
 
 export default App;
