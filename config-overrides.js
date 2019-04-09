ac932233ce43d44bd1eec4c368113a489678d25b
Jeff Auriemma <jdauriemma@gmail.com>
2019-04-09 16:25:56 -0400

    Update build to work in prod

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,6 @@
-const {
-  rewireBlockstackBuild,
-  rewireBlockstackDevServer,
-} = require('react-app-rewire-blockstack');
+const { rewireBlockstackDevServer } = require('react-app-rewire-blockstack');
 
 module.exports = {
-  webpack: (config, env) => {
-    if (env === 'production') {
-      config = rewireBlockstackBuild(config);
-    }
-    return config;
-  },
-
   devServer: (configFunction) => {
     return (proxy, allowedHost) => {
       let config = configFunction(proxy, allowedHost);
