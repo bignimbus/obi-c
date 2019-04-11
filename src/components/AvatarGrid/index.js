4e16bcdade5d114aecbe5a22485398d9dd3df741
Jeff Auriemma <jdauriemma@gmail.com>
2019-04-11 09:46:59 -0400

    Responsive grid system

--- src/components/AvatarGrid/index.js
+++ src/components/AvatarGrid/index.js
@@ -1,38 +1,57 @@
 import './index.css';
-import React, { Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import Avatar, { PIXELS_WIDE, PIXELS_TALL } from '../Avatar';
 
-const NARROW_SCREEN_PIXEL_COUNT = 40;
-const SHORT_SCREEN_PIXEL_COUNT = 67;
+const pixelCount = dimension => Math.floor(dimension * 0.1067);
 
 const AvatarGrid = () => {
+  const [pixelCountWide, setPixelCountWide] = useState(
+    pixelCount(window.innerWidth),
+  );
+
+  const [pixelCountTall, setPixelCountTall] = useState(
+    pixelCount(window.innerHeight),
+  );
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setPixelCountWide(pixelCount(window.innerWidth));
+      setPixelCountTall(pixelCount(window.innerHeight));
+    }, false);
+  });
+
+  const horizontalCenter= Math.floor((pixelCountWide - PIXELS_WIDE) / 2);
+  const verticalCenter = Math.floor((pixelCountTall - PIXELS_TALL) / 2);
+
   return (
     <div className='grid'>
       <div
         className='grid__avatar'
         style={{
-          height: `calc(100% / ${SHORT_SCREEN_PIXEL_COUNT} * ${PIXELS_TALL})`,
-          width: `calc(100% / ${NARROW_SCREEN_PIXEL_COUNT} * ${PIXELS_WIDE})`,
+          height: `${100 / pixelCountTall * PIXELS_TALL}%`,
+          left: `${100 / pixelCountWide * horizontalCenter}%`,
+          top: `${100 / pixelCountTall * verticalCenter}%`,
+          width: `${100 / pixelCountWide * PIXELS_WIDE}%`,
         }}
       >
         <Avatar />
       </div>
       {
-        new Array(SHORT_SCREEN_PIXEL_COUNT).fill(null).map((_, rowIndex) => (
+        new Array(pixelCountTall).fill(null).map((_, rowIndex) => (
           <div
             key={rowIndex}
             className='grid__row'
             style={{
-              height: `calc(100% / ${SHORT_SCREEN_PIXEL_COUNT})`,
+              height: `${100 / pixelCountTall}%`,
             }}
           >
             {
-              new Array(NARROW_SCREEN_PIXEL_COUNT).fill(null).map((_, colIndex) => (
+              new Array(pixelCountWide).fill(null).map((_, colIndex) => (
                 <div
                   key={colIndex}
                   className='grid__col'
                   style={{
-                    width: `calc(100% / ${NARROW_SCREEN_PIXEL_COUNT})`,
+                    width: `${100 / pixelCountWide}%`,
                   }}
                 />
               ))
